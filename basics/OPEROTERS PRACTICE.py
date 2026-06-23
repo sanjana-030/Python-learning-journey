cb4117e5f8107dd5e9cc55892acc8acce5a5c508
Sanjana <me.sanjana00@gmail.com>
2026-06-23 01:03:09 -0700

    Add files via upload

---
+++ basics/OPEROTERS PRACTICE.py
@@ -0,0 +1,70 @@
+"""
+PYTHONE OPERATOR'S
+a+b ,  a,b   Operand  , + => Operator
+
+1. Arithemetic Opertor
+( +  ,-  , *  ,/ ,// ,% ,**)
+   
+a = 4
+b = 10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+2. Assignment Operator
+  (=, += ,-= etc)
+  
+a =79
+print(a)
+a +=1
+print(a)
+a -=2
+print(a)
+
+
+3.Relational/ Conditional Operators ( Return Boolean Answer),True or false
+ > ,< , >=, <=, == !=
+
+
+a = 55
+b = 61
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+
+4.Membership Opertor ( Return Boolean)
+  Check Existance
+  (in , not in)
+
+a = "sanjana"
+b = "jana"
+print( a not in b)
+print(b in a)
+
+5. Indentity Operator (Return Boolean)
+  Check Exact Match
+  (is  ,is not)
+
+
+a = "sanjana"
+b = "jana"
+print(a is b)
+print(b is not a)
+
+6. Logical Operator
+( and , or , not)
+
+"""
+
+print( 1>6 or 10>7)
+print( 1>6 and 10>7)
